9bdd909d2530322bfba4aeb23549ab2d43f361e7
hideokamoto <hide.okamoto@digitalcube.jp>
2016-03-08 00:08:28 +0900

    get root api from wp-kyoto

--- index.ios.js
+++ index.ios.js
@@ -3,50 +3,80 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
+var REQUEST_URL = 'http://wp-kyoto.net/wp-json/';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	Component,
+	StyleSheet,
+	Text,
+	View
 } from 'react-native';
 
 class reactWpApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+	constructor(props) {
+			super(props);
+			this.state = {
+				site: [],
+			};
+		}
+	fetchData() {
+			fetch(REQUEST_URL)
+				.then((response) => response.json())
+				.then((responseData) => {
+					this.setState({
+						site: responseData,
+					});
+				})
+				.done();
+		}
+	renderLoadingView() {
+			return (
+				<View style={styles.container}>
+					<Text>
+						Loading posts...
+					</Text>
+				</View>
+			);
+		}
+	componentDidMount() {
+		this.fetchData();
+	}
+	render() {
+		var site = this.state.site
+		if (!site) {
+				return this.renderLoadingView();
+			}
+
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					{site.name}
+				</Text>
+				<Text style={styles.instructions}>
+					{site.description}
+				</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 AppRegistry.registerComponent('reactWpApp', () => reactWpApp);
