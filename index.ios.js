5f4a33ed8b2e7a8dfe8302943d0189618056e30c
hideokamoto <hide.okamoto@digitalcube.jp>
2016-03-09 00:05:42 +0900

    get list view

--- index.ios.js
+++ index.ios.js
@@ -3,80 +3,111 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-var REQUEST_URL = 'http://wp-kyoto.net/wp-json/';
+var REQUEST_URL = 'http://wp-kyoto.net/wp-json/wp/v2/posts/?_embed';
 import React, {
 	AppRegistry,
 	Component,
+	ListView,
 	StyleSheet,
 	Text,
-	View
+	View,
 } from 'react-native';
 
 class reactWpApp extends Component {
 	constructor(props) {
-			super(props);
-			this.state = {
-				site: [],
-			};
-		}
-	fetchData() {
-			fetch(REQUEST_URL)
-				.then((response) => response.json())
-				.then((responseData) => {
-					this.setState({
-						site: responseData,
-					});
-				})
-				.done();
-		}
-	renderLoadingView() {
-			return (
-				<View style={styles.container}>
-					<Text>
-						Loading posts...
-					</Text>
-				</View>
-			);
-		}
+		super(props);
+		this.state = {
+			dataSource: new ListView.DataSource({
+				rowHasChanged: (row1, row2) => row1 !== row2,
+			}),
+			loaded: false,
+		};
+	}
+
 	componentDidMount() {
 		this.fetchData();
 	}
-	render() {
-		var site = this.state.site
-		if (!site) {
-				return this.renderLoadingView();
-			}
 
+	fetchData() {
+		fetch(REQUEST_URL)
+			.then((response) => response.json())
+			.then((responseData) => {
+				console.log(responseData);
+				this.setState({
+					dataSource: this.state.dataSource.cloneWithRows(responseData),
+					loaded: true,
+				});
+			})
+			.done();
+	}
+
+	render() {
+		if (!this.state.loaded) {
+			return this.renderLoadingView();
+		}
+		return (
+			<ListView
+				dataSource={this.state.dataSource}
+				renderRow={this.renderPost}
+				style={styles.listView}
+			/>
+		);
+	}
+
+	renderLoadingView() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>
-					{site.name}
-				</Text>
-				<Text style={styles.instructions}>
-					{site.description}
+				<Text>
+					Loading posts...
 				</Text>
 			</View>
 		);
 	}
-}
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
-});
+	renderPost(post) {
+		var date = new Date(post.date_gmt).toLocaleDateString();
+		var author = post['_embedded']['author'][0]['name'];
+		return (
+			<View style={styles.container}>
+				<View style={styles.innerContainer}>
+					<Text style={styles.title}>{post.title.rendered}</Text>
+					<Text style={styles.postInfo}>{date}　{author}</Text>
+				</View>
+			 </View>
+			);
+		}
+	}
+
+	var styles = StyleSheet.create({
+		container: {
+			flex: 1,
+			flexDirection: 'row',
+			justifyContent: 'center',
+			alignItems: 'center',
+			backgroundColor: '#F5FCFF',
+			borderBottomWidth: 1,
+			borderBottomColor: '#dedede'
+		},
+		innerContainer: {
+			flex: 1,
+			marginRight: 20,
+			marginLeft: 20,
+			marginTop: 20,
+			marginBottom: 20,
+		},
+		title: {
+			fontSize: 20,
+			marginBottom: 8,
+			textAlign: 'center',
+		},
+		postInfo: {
+			fontSize: 10,
+			textAlign: 'center',
+		},
+		listView: {
+			paddingTop: 20,
+			backgroundColor: '#F5FCFF',
+		},
+	});
 
 AppRegistry.registerComponent('reactWpApp', () => reactWpApp);
